025d552d4eb366e7b407a99b90830ea1f73af370
alime008 <33568303+alime008@users.noreply.github.com>
2020-10-25 23:32:17 -0700

    Update

--- run.sh
+++ run.sh
@@ -3,10 +3,9 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python 3.7. Remember that in order to execute the following command, you need to uncomment the line
 
 SCRIPT_PATH="./src/population.py" 
-INPUT_PATH="./input/censustract-00-10.csv" 
-OUTPUT_PATH="./output/report.csv"
+INPUT_PATH="./insight_testsuite/tests/test_1/input/censustract-00-10.csv" 
+OUTPUT_PATH="./insight_testsuite/tests/test_1/output/report.csv"
 PYTHON=python3
 
 # call script via the interrupter     
 $PYTHON $SCRIPT_PATH $INPUT_PATH $OUTPUT_PATH
-## PLEASE OPEN THE POPULATION.PY FILE TO CHANGE THE INPUT AND OUTPUT FILE YOU WOULD LIKE TO RUN/TEST!
