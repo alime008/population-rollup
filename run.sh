9ca73ccf452aeac273e0142c4e776390c3854257
alime008 <33568303+alime008@users.noreply.github.com>
2020-10-25 22:40:16 -0700

    Update

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python 3.7. Remember that in order to execute the following command, you need to uncomment the line
 
-SCRIPT_PATH=C:/Users/ali.mahdavi/Documents/GitHub/population-rollup/src/population.py 
+SCRIPT_PATH="C:/Users/ali.mahdavi/Documents/GitHub/population-rollup/src/population.py" 
 PYTHON=python
 
 # call script via the interrupter     
