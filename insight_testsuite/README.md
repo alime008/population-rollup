3b9ee58e849f79e39341bd12e02c482dee401ed8
alime008 <33568303+alime008@users.noreply.github.com>
2020-10-25 20:57:51 -0700

    MySolution
    I'm not an expert on workign with Git or using shell scripts. I've spent a lot of time trying to figure out what I can. Hoping this satisfies some requirments. I'm very excited to have the opportunity to work with Insight Fellows. Nevertheless, I appreciate this project to allow me to learn that I'm capable.

--- insight_testsuite/README.md
+++ insight_testsuite/README.md
@@ -1,1 +1,6 @@
 This is the directory where you'd place your test cases. One test, test_1, has been provided for your use.
+
+test_1 is satisfied with the python code named "population_test.py". This runs the test in test_1 and creates the report file.
+
+
+I've not been able to input the path from the shell script so I have to change the path in my python code manually. 
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python 3.7. Remember that in order to execute the following command, you need to uncomment the line
-# python3.7 ./src/population.py ./input/censustract-00-10.csv ./output/report.csv
+Python C:/Users/ali.mahdavi/Documents/GitHub/population-rollup/src/population.py
+
+## PLEASE OPEN THE POPULATION.PY FILE TO CHANGE THE INPUT AND OUTPUT FILE YOU WOULD LIKE TO RUN/TEST!
