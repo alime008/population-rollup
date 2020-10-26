51c747cbf404415b433eab635d87f64f919e6b8b
alime008 <33568303+alime008@users.noreply.github.com>
2020-10-25 21:43:50 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env python3
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python 3.7. Remember that in order to execute the following command, you need to uncomment the line
 
-#!/C:/Python
 Python C:/Users/ali.mahdavi/Documents/GitHub/population-rollup/src/population.py
 
 ## PLEASE OPEN THE POPULATION.PY FILE TO CHANGE THE INPUT AND OUTPUT FILE YOU WOULD LIKE TO RUN/TEST!
